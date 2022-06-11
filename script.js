3293ec918a6b445c44f6da0576780d5444c000cc
Edvardsen <petter-edvardsen@hotmail.com>
2022-06-11 12:07:36 +0100

    Finalized the layout and animations of the webpage.

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
 
-
 $(document).ready(function(){
-    var firstIcon = $("day1");
+    var dayGrid = document.querySelector('.days');
+    var firstIcon = document.querySelector('.day1');
+    var secondIcon = document.querySelector('.day2');
+    var thirdIcon = document.querySelector('.day3');
+    var fourthIcon = document.querySelector('.day4');
+    var fifthIcon = document.querySelector('.day5');
+    var sixthIcon = document.querySelector('.day6');
+    var seventhIcon = document.querySelector('.day7');
 
-    firstIcon.append('<div class="sunny"></div>');
+
+    firstIcon.classList.add('sunny');
+    secondIcon.classList.add('sunny');
+    thirdIcon.classList.add('cloudy');
+    fourthIcon.classList.add('rainy');
+    fifthIcon.classList.add('rainy');
+    sixthIcon.classList.add('stormy');
+    seventhIcon.classList.add('cloudy');
+
+
+  
+    //dayGrid.firstChild.replaceWith(firstIcon);
+    
+
+    //dayGrid.classList.add(firstIcon);
+    //secondIcon.classList.add('sunny');
+    //thirdIcon.classList.add('cloudy');
+
 });
+
+function showContent() {
+  var temp = document.getElementsByTagName("template")[0];
+  var clon = temp.content.cloneNode(true);
+  document.body.appendChild(clon);
+}; 
